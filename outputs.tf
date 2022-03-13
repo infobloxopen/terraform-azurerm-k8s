0ff1ebbdd5b74c6abf5bb57d977bcab25a3de6ff
Phillip Ferrell <pferrell@infoblox.com>
2022-03-13 17:26:34 -0500

    refactor to remove count since this is handled at higher-level module

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "kubeconfig_path_aks" {
-  value = local_file.kubeconfigaks.0.filename
+output "kubeconfig_path" {
+  value = local_file.kubeconfig.filename
 }
 
 output "latest_k8s_version" {
-  value = data.azurerm_kubernetes_service_versions.current.*.latest_version
+  value = data.azurerm_kubernetes_service_versions.current.latest_version
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.0.ip_address
+  value = azurerm_public_ip.public_ip.ip_address
 }
 
 output "public_ip_fqdn" {
-  value = azurerm_public_ip.public_ip.0.fqdn
+  value = azurerm_public_ip.public_ip.fqdn
 }
